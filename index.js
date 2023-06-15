0b67a4bca9e707bf76f7094837ea472c7771840d
wahyu <komang01966@gmail.com>
2023-06-15 19:51:13 +0800

    Init express

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors');
+const morgan = require('morgan');
+const session = require('express-session');
+const flash = require('express-flash');
+const path = require('path')
+// const passport = require('passport');
+
+
+const app = express()
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.listen(3000, () => {
+    console.log("Running on localhost:3000");
+  });
