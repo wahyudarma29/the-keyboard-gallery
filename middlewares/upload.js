9b254e54460e2803caeb570fb5500c84383b95cc
wahyu <komang01966@gmail.com>
2023-06-21 00:55:57 +0800

    Add upload middleware

---
+++ middlewares/upload.js
@@ -0,0 +1,22 @@
+const multer = require("multer");
+const mime = require("mime-types");
+const path = require("path");
+
+const multerStorageData = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../public/uploads"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      `${file.fieldname}-${uniqueSuffix}.${mime.extension(file.mimetype)}`
+    );
+  },
+});
+
+const upload = multer({ storage: multerStorageData });
+
+module.exports = upload
+
+// from the last material
