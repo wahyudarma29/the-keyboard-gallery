76c72d3d70d5da7b9431cd15c24bda7f75631d12
wahyu <komang01966@gmail.com>
2023-06-21 23:47:31 +0800

    Update route

--- routers/board.router.js
+++ routers/board.router.js
@@ -6,9 +6,10 @@ const upload = require("../middlewares/upload")
 const router = Router();
 
 router.get("/", BoardController.listPage);
-router.get("/board/create", BoardController.createPage);
+router.get("/board/add", BoardController.createPage);
 router.get("/board/:id/edit", BoardController.updatePage);
+router.get("/board/:id/details", BoardController.detailPage);
 router.post("/board", upload.single('img'), BoardController.addNew);
-
+router.post("/board/:id/update", upload.single('img'), BoardController.update);
 
 module.exports = router;
