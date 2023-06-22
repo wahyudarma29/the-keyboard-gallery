923cba465e81a65b6a56c7453f255dced9de03b1
wahyu <komang01966@gmail.com>
2023-06-22 22:48:11 +0800

    Add route to like and list liked, change order

--- routers/board.router.js
+++ routers/board.router.js
@@ -1,16 +1,21 @@
 const { Router } = require("express");
 
 const BoardController = require("../controllers/board.controller")
+
 const upload = require("../middlewares/upload")
 
 const router = Router();
 
 router.get("/", BoardController.listPage);
+router.get("/", BoardController.userBoardList);
 router.get("/board/add", BoardController.createPage);
-router.get("/board/:id/edit", BoardController.updatePage);
 router.get("/board/:id/details", BoardController.detailPage);
-router.post("/board", upload.single('img'), BoardController.addNew);
-router.post("/board/:id/update", upload.single('img'), BoardController.update);
+router.get("/board/:id/edit", BoardController.updatePage);
 router.get("/my-gallery", BoardController.userBoardList);
+router.get("/my-favorites", BoardController.likedBoardList);
+
+router.post("/board", upload.single('img'), BoardController.addNew);
+router.post("/board/:id/like", BoardController.likeBoard);
+router.post("/board/:id/update", upload.single('img'), BoardController.update);
 
 module.exports = router;
